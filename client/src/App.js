6d4a8b3b242be92362f474cf8310258aab4d302b
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-22 01:48:36 +0530

    done with header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./components/header/header";
-
+import Note from "./components/note/note";
+import Footer from "./components/footer/footer";
 const App = () => {
   return (
     <>
       <div>
         <Header />
+        <Note />
+        <Footer />
       </div>
     </>
   );
---
+++ client/src/components/note/note.js
@@ -0,0 +1,12 @@
+const Note = () => {
+  return (
+    <>
+      <div className="note">
+        <h1>Title</h1>
+        <p>content</p>
+      </div>
+    </>
+  );
+};
+
+export default Note;
