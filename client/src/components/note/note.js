4e8277e23b104992b2a1c33af916d299c6259394
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-09-03 10:32:15 +0530

    speech recognition added

--- client/src/components/note/note.js
+++ client/src/components/note/note.js
@@ -2,12 +2,39 @@ import DeleteIcon from "@material-ui/icons/Delete";
 
 const Note = (props) => {
   const { id, title, content, deleteNote } = props;
-
   console.log(title);
+
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new window.SpeechRecognition();
+  recognition.interimResults = true;
+  recognition.lang = "en-US";
+
+  // let p = document.createElement("p");
+  // const words = document.querySelector(".words");
+  // words.appendChild(p);
+
+  recognition.addEventListener("result", (e) => {
+    const transcript = Array.from(e.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join("");
+
+    console.log(transcript);
+
+    // if (e.results[0].isFinal) {
+    //   p = document.createElement("p");
+    //   words.appendChild(p);
+    // }
+  });
+
+  recognition.addEventListener("end", recognition.start);
+  recognition.start();
 
   return (
     <>
-      <div className="note">
+      <div className="note" contenteditable>
         <h1>{title}</h1>
         <p>{content}</p>
         <button
